dbfd80c1fcefa8ebfe55fcc512437c30d794dd43
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2023-12-06 00:10:55 -0300

    finished migration from js to ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import { IColaborador } from './shared/interfaces/IColaborador';
 
 function App() {
 
@@ -43,16 +44,15 @@ function App() {
     }
   ]
 
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState<IColaborador[]>([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
+  const aoNovoColaboradorAdicionado = (colaborador: IColaborador) => {
     setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
-      <Banner enderecoImagem="/imagens/banner.png" alt="O banner principal da página do Organo" />
+      <Banner enderecoImagem="/imagens/banner.png" textoAlternativo="O banner principal da página do Organo" />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
 
       {times.map(time => <Time 
